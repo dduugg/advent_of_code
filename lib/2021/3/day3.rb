0195d883ef4b4871984b41f59a2368432b486c98
Douglas Eichelberger <dduugg@gmail.com>
2021-12-03 17:50:38 -0800

    day3

---
+++ lib/2021/3/day3.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+# typed: strict
+# frozen_string_literal: true
+
+input = File.readlines("#{__dir__}/input").map(&:chomp)
+width = input[0].size
+acc = Array.new(width, 0)
+gamma = input.each_with_object(acc) do |line, memo|
+  line.chars.each_with_index.map do |char, index|
+    char == '1' ? memo[index] += 1 : memo[index] -= 1
+  end
+end.map { |i| i.positive? ? 1 : 0 }
+epsilon = gamma.map { _1 ^ 1}
+gamma = gamma.join.to_i(2)
+epsilon = epsilon.join.to_i(2)
+puts gamma * epsilon
+
+def get_rating_using(comparator)
+  rating_lines = File.readlines("#{__dir__}/input").map(&:chomp)
+  rating = []
+  (0...rating_lines[0].size).each do |i|
+    parts = rating_lines.partition { _1[i] == '0' }
+    if parts[0].size.public_send(comparator, parts[1].size)
+      rating[i] = 0
+      rating_lines = parts[0]
+    else
+      rating[i] = 1
+      rating_lines = parts[1]
+    end
+    break if rating_lines.size == 1
+  end
+  rating_lines.first
+end
+
+oxygen_generator_rating = get_rating_using(:>)
+co2_scrubber_rating = get_rating_using(:<=)
+ogr = oxygen_generator_rating.to_i(2)
+csr = co2_scrubber_rating.to_i(2)
+puts ogr * csr
+# 2950146 is too low
